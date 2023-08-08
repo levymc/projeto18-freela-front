20ee0fc55b863bbfd025ba30f77e520cfbd89b27
Levy <levymcruz@gmail.com>
2023-08-08 20:39:07 -0300

    aa

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,94 @@
+import React, { createContext, useContext, useState } from 'react';
+import styled from 'styled-components';
+import { Close, ChevronForwardOutline, ChevronDownOutline } from 'react-ionicons';
+import { useAuth } from './AuthContext';
+import Collapse from 'react-bootstrap/Collapse';
+
+export default function SideBar() {
+    const { telaAcesso, setTelaAcesso, logado, setLogado, isOpen, setIsOpen } = useAuth();
+    const [open, setOpen] = useState(false);
+
+    const handleCloseSideBar = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <SideBarContainer isOpen={isOpen}>
+            <SecIcon>
+                <Close
+                    onClick={handleCloseSideBar}
+                    height="30px" width="30px"
+                    color={`#FFF4F4`}
+                />
+            </SecIcon>
+            <Button
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+            >
+                Botão 1 {open  
+                            ? <SecionArrowIcon><ChevronDownOutline height="30px" width="30px"color={`#FFF4F4`} /></SecionArrowIcon>
+                            : <SecionArrowIcon><ChevronForwardOutline height="30px" width="30px"color={`#FFF4F4`} /></SecionArrowIcon>
+                        }
+            </Button>
+            <Collapse in={open}>
+                <CollapsedDiv id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                </CollapsedDiv>
+            </Collapse>
+            <Button>Botão 2</Button>
+            <Button>Botão 3</Button>
+            <Button last={true}>Botão 4</Button>
+        </SideBarContainer>
+    );
+}
+
+const SideBarContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: ${props => (props.isOpen ? '0' : '-18em')};
+    width: 18em;
+    height: 100vh;
+    background-color: RGB(255, 101, 0);
+    transition: left 0.3s ease-in-out; 
+    z-index: 1000; 
+    /* padding-left: 1em; */
+    padding-top: 7em;
+    display: flex;
+    flex-direction: column;
+    /* gap: 1em; */
+    align-items: center;
+    /* position: relative; */
+`;
+
+const SecIcon = styled.section`
+    position: absolute;
+    top: 2em;
+    right: 2em;
+`;
+
+const Button = styled.button`
+    width: 100%;
+    background-color: transparent;
+    border-top: 1px solid #fafafb6c; 
+    border-left: 0; border-right: 0;
+    border-radius: 0;
+    border-bottom: ${props => (props.last ? `1px solid #fafafb6c` : '0')}; 
+    position: relative;
+    display: flex;
+    align-items: center;
+
+`;
+
+const SecionArrowIcon = styled.section`
+    position: absolute;
+    right: 1em;
+`
+
+const CollapsedDiv = styled.div`
+    padding: 1.2em;
+    border-top: 1px solid #fafafb6c; 
+    color: #dadada;
+`
---
+++ src/routes/Login.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import Carousel from 'react-bootstrap/Carousel'
+import { useAuth } from '../components/AuthContext';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function Home() {
+	const { telaAcesso, setTelaAcesso, logado, setLogado } = useAuth();
+
+	return (
+		<Div height={'100vh'}>
+			<Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                        </Form.Group>
+                
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+			</Body>
+		</Div>
+	);
+}
+
+
+
+const Div = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: ${props => props.height};
+	background-color: RGB(0, 26, 61);
+`;
+
+const Body = styled.div`
+    position: absolute;
+    top: 14em;
+	display: flex;
+    justify-content: center;
+	flex-direction: column;
+    align-items: center;
+    height: 40vh;
+    width: 30vw;
+    background-color: RGB(250, 250, 251);
+	box-shadow: 1px 1px 4px 4px rgba(170, 170, 170, 0.212);
+    margin: 0 auto;
+    padding: 20px; 
+    border-radius: 10px;
+    color: black;
+	.imgSlide{
+		width: 900px !important;
+		height: 500px !important;
+	}
+`;
+
+// Não é necessário importar ou definir o componente ExampleCarouselImage.
